90712bd8a5b268e140c40193b4e74d1fc7276543
ktksan <kartikeya.sanwal@gmail.com>
2020-06-19 18:58:59 +0530

    Add ChangeMaxHealthEvent/AuthoritySystem

--- src/main/java/org/terasology/logic/health/ChangeMaxHealthAuthoritySystem.java
+++ src/main/java/org/terasology/logic/health/ChangeMaxHealthAuthoritySystem.java
@@ -5,27 +5,28 @@ package org.terasology.logic.health;
 
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.logic.health.event.ChangeMaxHealthEvent;
-import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
-import org.terasology.rendering.nui.ControlWidget;
+import org.terasology.registry.In;
 import org.terasology.rendering.nui.NUIManager;
+import org.terasology.rendering.nui.layers.hud.HealthHud;
 import org.terasology.rendering.nui.widgets.UIIconBar;
 
-public class ChangeMaxHealthAuthoritySystem {
+@RegisterSystem(value = RegisterMode.AUTHORITY)
+public class ChangeMaxHealthAuthoritySystem extends BaseComponentSystem {
+    @In
     NUIManager nuiManager;
+    @In
+    HealthHud healthHud;
 
     @ReceiveEvent
-    public void changeMaxHealth(ChangeMaxHealthEvent event, EntityRef player,HealthComponent health) {
-        ControlWidget healthHUD = nuiManager.getHUD().getHUDElement("healthHUD");
-        UIIconBar healthBar = healthHUD.find("healthBar", UIIconBar.class);
-        healthBar.setMaxIcons(event.getNewMaxHealth()/10);
+    public void changeMaxHealth(ChangeMaxHealthEvent event, EntityRef player, HealthComponent health) {
+        health.maxHealth = event.getNewMaxHealth();
+        player.saveComponent(health);
+        UIIconBar healthBar = nuiManager.getHUD().find("healthBar", UIIconBar.class);
+        healthBar.setMaxIcons(event.getNewMaxHealth() / 10);
     }
 
-
-    @ReceiveEvent
-    public void onPlayerSpawn(OnPlayerSpawnedEvent event, EntityRef player, HealthComponent health) {
-        ControlWidget healthHUD = nuiManager.getHUD().getHUDElement("healthHUD");
-        UIIconBar healthBar = healthHUD.find("healthBar", UIIconBar.class);
-        healthBar.setMaxIcons(5);
-    }
 }
--- src/main/java/org/terasology/logic/health/event/ChangeMaxHealthEvent.java
+++ src/main/java/org/terasology/logic/health/event/ChangeMaxHealthEvent.java
@@ -5,6 +5,9 @@ package org.terasology.logic.health.event;
 
 import org.terasology.entitySystem.event.Event;
 
+/**
+ * This Event is sent out whenever a system wants to alter the maxHealth of an entity.
+ */
 public class ChangeMaxHealthEvent implements Event {
     private int newMaxHealth;
 
