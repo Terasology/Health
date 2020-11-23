64f59697d2ed6d93510376e237058b6744a74bae
Adam Kneller <a_kneller@yahoo.com.au>
2020-11-24 06:17:11 +1000

    Add a red flash on receiving damage

--- src/main/java/org/terasology/logic/health/HealthClientSystem.java
+++ src/main/java/org/terasology/logic/health/HealthClientSystem.java
@@ -15,20 +15,54 @@
  */
 package org.terasology.logic.health;
 
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.delay.DelayManager;
+import org.terasology.logic.delay.DelayedActionTriggeredEvent;
+import org.terasology.logic.health.event.OnDamagedEvent;
+import org.terasology.logic.players.PlayerCharacterComponent;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.NUIManager;
+import org.terasology.rendering.nui.layers.hud.DamagedOverlay;
 
 @RegisterSystem(RegisterMode.CLIENT)
 public class HealthClientSystem extends BaseComponentSystem {
 
+    private static final String REMOVE_DAMAGED_OVERLAY_ACTION = "Health:RemoveDamagedOverlay";
+    private static final String DAMAGED_OVERLAY = "damagedOverlay";
+    private static final long DAMAGED_OVERLAY_DELAY_MS = 150L;
+
     @In
     private NUIManager nuiManager;
+
+    @In
+    private DelayManager delayManager;
+
+    private boolean overlayDisplaying = false;
 
     @Override
     public void initialise() {
         nuiManager.getHUD().addHUDElement("healthHud");
     }
+
+    @ReceiveEvent(components = PlayerCharacterComponent.class)
+    public void onDamaged(OnDamagedEvent event, EntityRef entity) {
+        if (!overlayDisplaying && event.getDamageAmount() > 0) {
+            overlayDisplaying = true;
+            nuiManager.addOverlay(DAMAGED_OVERLAY, DamagedOverlay.class);
+            delayManager.addDelayedAction(entity, REMOVE_DAMAGED_OVERLAY_ACTION, DAMAGED_OVERLAY_DELAY_MS);
+        }
+    }
+
+    @ReceiveEvent
+    public void onDelayedAction(DelayedActionTriggeredEvent event, EntityRef entityRef) {
+        if (event.getActionId().equals(REMOVE_DAMAGED_OVERLAY_ACTION)) {
+            nuiManager.removeOverlay(DAMAGED_OVERLAY);
+            overlayDisplaying = false;
+        }
+    }
+
 }
---
+++ src/main/java/org/terasology/rendering/nui/layers/hud/DamagedOverlay.java
@@ -0,0 +1,11 @@
+package org.terasology.rendering.nui.layers.hud;
+
+import org.terasology.rendering.nui.CoreScreenLayer;
+
+public class DamagedOverlay extends CoreScreenLayer {
+
+    @Override
+    public void initialise() {
+
+    }
+}
