f3b2e321bc9fc46d08dc10d8cd68b58f0d9c4645
Josephine Rueckert <jd.rueckert@googlemail.com>
2020-12-13 13:54:51 +0100

    fix: remove unnecessary vector conversions

--- src/main/java/org/terasology/logic/health/DamageAuthoritySystem.java
+++ src/main/java/org/terasology/logic/health/DamageAuthoritySystem.java
@@ -220,7 +220,7 @@ public class DamageAuthoritySystem extends BaseComponentSystem {
      */
     @ReceiveEvent
     public void onCrash(HorizontalCollisionEvent event, EntityRef entity, HealthComponent health) {
-        Vector3f vel = new Vector3f(JomlUtil.from(event.getVelocity()));
+        Vector3f vel = new Vector3f(event.getVelocity());
         vel.y = 0;
         float speed = vel.length();
 
@@ -247,7 +247,7 @@ public class DamageAuthoritySystem extends BaseComponentSystem {
     @ReceiveEvent
     public void onCrash(HorizontalCollisionEvent event, EntityRef entity, CharacterSoundComponent characterSounds,
                         HealthComponent healthComponent) {
-        Vector3f horizVelocity = new Vector3f(JomlUtil.from(event.getVelocity()));
+        Vector3f horizVelocity = new Vector3f(event.getVelocity());
         horizVelocity.y = 0;
         float velocity = horizVelocity.length();
 
