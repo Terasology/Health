7c7996793634ccb36f3c15351c4b593b4aa1be0b
Tobias Nett <skaldarnar@googlemail.com>
2021-05-01 22:11:15 +0200

    chore!: remove unused fields from components
    BREAKING CHANGE: This finalizes the split of health-related components by removing unused fields. Replacements can be found in RegenComponent or BaseRegenComponent.

--- src/main/java/org/terasology/module/health/components/RegenComponent.java
+++ src/main/java/org/terasology/module/health/components/RegenComponent.java
@@ -2,9 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.module.health.components;
 
-import com.google.common.collect.Ordering;
-import com.google.common.collect.SortedSetMultimap;
-import com.google.common.collect.TreeMultimap;
 import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
 import org.terasology.module.health.events.ActivateRegenEvent;
@@ -16,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * [INTERNAL] This component is managed by {@link org.terasology.module.health.systems.NewRegenAuthoritySystem}.
+ * [INTERNAL] This component is managed by {@link org.terasology.module.health.systems.RegenAuthoritySystem}.
  *
  * <p>
  * To register an indefinite regeneration action in a prefab you can give it a regen component and pre-fill the
@@ -41,29 +38,10 @@ import java.util.Map;
  */
 public class RegenComponent implements Component {
     /**
-     * The timestamp in in-game time (ms) when the next regeneration action ends.
-     */
-    @Replicate
-    public long soonestEndTime = Long.MAX_VALUE;
-
-    /**
-     * Mapping from regeneration action ids to the regeneration value.
-     */
-    @Replicate
-    public Map<String, Float> regenValue = new HashMap<>();
-
-    /**
-     * Registered regeneration action ids associated to their end time.
-     */
-    @Replicate
-    public SortedSetMultimap<Long, String> regenEndTime = TreeMultimap.create(Ordering.natural(),
-            Ordering.arbitrary());
-
-    /**
      * The decimal place of the last regen tick before rounding the amount to integer.
      * <p>
      * This is to compensate for inaccuracy when computing the regeneration amount in integers. The remainder is picked
-     * up at the next iteration of the {@link org.terasology.module.health.systems.NewRegenAuthoritySystem}.
+     * up at the next iteration of the {@link org.terasology.module.health.systems.RegenAuthoritySystem}.
      */
     @Replicate
     public float remainder;
