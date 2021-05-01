520a02327c56787ea95d38dfd5b75144b8df3024
Tobias Nett <skaldarnar@googlemail.com>
2021-05-01 21:59:37 +0200

    feat: add registered 'actions' to RegenComponent and improve doc

--- src/main/java/org/terasology/module/health/components/RegenComponent.java
+++ src/main/java/org/terasology/module/health/components/RegenComponent.java
@@ -9,12 +9,32 @@ import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
 import org.terasology.module.health.events.ActivateRegenEvent;
 import org.terasology.module.health.events.DeactivateRegenEvent;
+import org.terasology.module.health.time.Instant;
+import org.terasology.naming.Name;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Not for direct access! Use regen events instead.
+ * [INTERNAL] This component is managed by {@link org.terasology.module.health.systems.NewRegenAuthoritySystem}.
+ *
+ * <p>
+ * To register an indefinite regeneration action in a prefab you can give it a regen component and pre-fill the
+ * registered actions with the respective id. To denote an indefinite timestamp, i.e., a never ending action, use a
+ * negative value (usually {@code -1}).
+ * <p>
+ * For instance, to pre-register the base regeneration with id {@code health:baseRegen} include the following in your
+ * prefab:
+ *
+ * <pre>
+ * {
+ *   "Regen": {
+ *     "actions": [
+ *       { "key": "health:baseRegen", "value": -1 }
+ *     ]
+ *   }
+ * }
+ * </pre>
  *
  * @see ActivateRegenEvent
  * @see DeactivateRegenEvent
@@ -39,6 +59,18 @@ public class RegenComponent implements Component {
     public SortedSetMultimap<Long, String> regenEndTime = TreeMultimap.create(Ordering.natural(),
             Ordering.arbitrary());
 
+    /**
+     * The decimal place of the last regen tick before rounding the amount to integer.
+     * <p>
+     * This is to compensate for inaccuracy when computing the regeneration amount in integers. The remainder is picked
+     * up at the next iteration of the {@link org.terasology.module.health.systems.NewRegenAuthoritySystem}.
+     */
     @Replicate
     public float remainder;
+
+    /**
+     * Registered regeneration action ids and their expiration timestamp.
+     */
+    @Replicate
+    public Map<Name, Instant> actions = new HashMap<>();
 }
